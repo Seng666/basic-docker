e9153aeb0c6006b55f2812ca1fde61a4cdd1039e
Jerryco <shoixshoix@gmail.com>
2020-07-26 09:36:26 +0800

    add backend files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,56 @@
 `use strict`
-var koa = require('koa');
+var Koa = require('koa');
 var Router = require('koa-router');
+const { MongoClient } = require('mongodb')
 
 var app = new Koa();
 var router = new Router();
 
-router.get('/', (ctx, next) => {
-  // ctx.router available
-});
+router
+  .get('/insert/:name', async (ctx, next) => {
+    // insert name parameter to db
+    const { name } = ctx.params
+
+    const db = mongo(ctx)
+    const collection = db.collection('names')
+
+    // insert name to the collection
+    await collection.insertOne({ name })
+
+    ctx.status = 200
+    ctx.body = name
+  })
+  .get('/names', async (ctx, next) => {
+    // get all logged names
+    const db = mongo(ctx)
+    const collection = db.collection('names')
+    const names = await collection.find().toArray()
+
+    ctx.body = names
+    ctx.status = 200
+  })
 
 app
+  .use(postgresMiddleware())
   .use(router.routes())
   .use(router.allowedMethods());
- 
-app.listen(3000);
+
+app.listen(3000)
+
+function postgresMiddleware() {
+  const mongoUrl = 'mongodb://mongoadmin:secret@localhost:27017'
+  const dbName = 'mydb1'
+  let client = undefined
+  return async (ctx, next) => {
+    if (client === undefined)
+      client = await MongoClient.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true })
+    
+    // attach db object to context
+    ctx._db = client.db(dbName)
+    return await next()
+  }
+}
+
+function mongo(ctx) {
+  return ctx._db
+}
